676331244da7ca3f30f3410184d9e2cad8d8c9f2
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-01 21:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function activateSearch() {
+  const searchDiv = document.querySelector('.search');
+  const inputElement = document.querySelector('.input');
+
+  searchDiv.classList.add('active');
+  inputElement.focus();
+}
